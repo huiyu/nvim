042f8c35d875a501b59368ddf2a6cbc446849206
Yu Hui <huiyu219@gmail.com>
2022-05-06 23:30:15 +0800

    compatible with new version of nvim according to https://github.com/Allaman/nvim/commit/b5588696cbf8ad0c84047b7ee04b2545cdd5d623

--- lua/user/lsp/config/gopls.lua
+++ lua/user/lsp/config/gopls.lua
@@ -2,10 +2,10 @@ local opts = {
 
 	on_attach = function(client, _)
 		-- disable internal formatting capability, using null-ls instead
-		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
+		client.server_capabilities.document_formatting = false
+		client.server_capabilities.document_range_formatting = false
 
-		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
 	end,
 }
 
--- lua/user/lsp/config/sumneko_lua.lua
+++ lua/user/lsp/config/sumneko_lua.lua
@@ -1,24 +1,24 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
 return {
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
+        version = "LuaJIT",
         -- Setup your lua path
         path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { "vim" },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
+        library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
@@ -32,11 +32,10 @@ return {
   },
   on_attach = function(client, bufnr)
     -- 禁用格式化功能，交给专门插件插件处理
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.document_range_formatting = false
 
     -- 保存时自动格式化
-    vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
   end,
 }
-
